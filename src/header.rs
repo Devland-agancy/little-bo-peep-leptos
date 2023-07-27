508c55a57d20803d92f3effe058cec179574f5dd
bokubeam <bokubeam@gmail.com>
2023-07-27 09:32:37 -0600

    Increase y-padding in menu

--- src/header.rs
+++ src/header.rs
@@ -66,7 +66,7 @@ fn MenuOpen(cx: Scope) -> impl IntoView {
             <MenuButton/>
         </div>
         <div class="w-full z-50 fixed right-0 top-14 flex self-start font-baskerville text-xl leading-3 md:leading-5 select-none">
-            <div class="absolute right-0 w-2/3 max-w-xs h-screen z-40 px-4 py-2 bg-stone-100">
+            <div class="absolute right-0 w-2/3 max-w-xs h-screen z-40 px-4 py-3 bg-stone-100">
                 <h2 class="font-baskerville-italic text-xl md:text-2xl pb-2">"Chapters"</h2>
                 <MenuItems />
             </div>
