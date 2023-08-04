78ff4ba70966a846408ba687a756a532173a520a
bokubeam <bokubeam@gmail.com>
2023-08-04 10:17:25 -0600

    Increase line-height on home links for large screens

--- src/page/home.rs
+++ src/page/home.rs
@@ -15,7 +15,7 @@ fn ArticleBody(cx: Scope) -> impl IntoView {
     view! {cx,
         <Columns>
             <Paragraph>
-                <ul class="leading-9 text-2xl lg:text-3xl">
+                <ul class="leading-9 lg:leading-10 text-2xl lg:text-3xl">
                     <li>
                          <Link href="article/ch_1">
                             "Chapter 1: A Few Refreshers"
