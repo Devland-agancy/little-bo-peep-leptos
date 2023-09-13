0370fc5c71896816b2ca179440b4a3deada9099f
Chaker Atallah <mr.chakerw@gmail.com>
2023-09-13 20:49:42 +0100

    Add meta tag to fix mobile scrolling

--- src/app.rs
+++ src/app.rs
@@ -33,7 +33,7 @@ pub fn App(cx: Scope) -> impl IntoView {
         // injects a stylesheet into the document <head>
         // id=leptos means cargo-leptos will hot-reload this stylesheet
         <Stylesheet id="leptos" href="/pkg/little-bo-peep.css"/>
-
+        <meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1" />
         // sets the document title
         <Title text="Little Bo Peep"/>
         <Link href="/images/book_favicon_sized_v2.png" rel="icon" />
