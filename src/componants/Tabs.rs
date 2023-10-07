95c45f588920d757ee10c284729102b17204e219
Chaker Atallah <mr.chakerw@gmail.com>
2023-10-07 22:38:41 +0100

    Style tabs and change showing selected tab content mechanism

--- src/componants/Tabs.rs
+++ src/componants/Tabs.rs
@@ -1,39 +1,31 @@
 use leptos::{*,  html::Div};
 use web_sys::Node;
+
+fn render_labels(cx: Scope, vec: &Vec<&'static str>, selected_tab: ReadSignal<usize>, set_selected_tab: WriteSignal<usize>) -> leptos::View {
+    let res = vec.into_iter()
+    .enumerate()
+    .map(|(i, el)| {
+        view! {cx,  <div  on:click=move |_|
+                set_selected_tab(i)
+          class="tab cursor-pointer px-4 py-2 rounded border-2 border-black"
+          class=("active", move || selected_tab() == i ) >{*el}</div>
+        }
+    })
+    .collect_view(cx);
+    res
+}
+
 #[component]
 pub fn tabs(cx: Scope, labels: Vec<&'static str>,children: ChildrenFn) -> impl IntoView 
 {
     let (selected_tab, set_selected_tab) = create_signal(cx, 0);
     
-    let lables = labels
-                            .into_iter()
-                            .enumerate()
-                            .map(|(i, el)| {
-                                view! {cx,  <div  on:click=move |_|{
-                                        set_selected_tab(i);
-                                        let maths = document().get_elements_by_class_name("hidden-on-startup");
-                                        /*for i in 0..maths.length() {
-                                            let _ = maths.item(i).unwrap().class_list().remove_1("hidden-on-startup");
-                                            let _ = maths.item(i).unwrap().class_list().add_1("animate-appear");
-                                             let load_math = r#"MathJax.Hub.Queue(["Typeset",MathJax.Hub]);"#;
-                                            let mut script = document().create_element("script");
-                                            script.as_mut().unwrap().set_inner_html(load_math);
-                                            document().head().unwrap().append_child(&script.unwrap() as &Node);
-                                        } */
-                                       
-                                    }
-                                  class="math_reloader cursor-pointer hover:font-bold"
-                                  class=("selected", move || selected_tab() == i ) >{el}</div>
-                                }
-                            })
-                            .collect_view(cx);
-
+    let lables = render_labels(cx, &labels, selected_tab, set_selected_tab);
+    let labeles_bottom = render_labels(cx, &labels, selected_tab, set_selected_tab);
    
     
     view! {cx,
-        <div
-            class="text-xl flex items-center justify-center gap-10 col-start-2 hidden-on-startup"
-        >
+        <div class="text-xl flex items-center justify-center gap-2 col-start-2 hidden-on-startup mb-10">
             {lables} 
         </div>
         <For
@@ -41,12 +33,19 @@ pub fn tabs(cx: Scope, labels: Vec<&'static str>,children: ChildrenFn) -> impl I
                 .nodes
                 .into_iter()
                 .enumerate()
-                .filter(move |f| f.0 == selected_tab())
+            /*     .filter(move |f| f.0 == selected_tab()) */
             key=|label| label.0
             view=move |cx, label| {
-                view! {cx, {label.1} }
+                view! {cx,
+                     <div class="col-start-2 transition-[height] duration-1000 overflow-y-clip      relative"
+                        class=("hidden", move || selected_tab() != label.0)> {label.1} 
+                     </div> 
+                    }
             }
         />
+        <div class="text-xl flex items-center justify-center gap-2 col-start-2 hidden-on-startup">
+            {labeles_bottom} 
+        </div>
     }
 }
 
