6de8d44aeac0e42767b3408c5f13d719d69ea6ca
Chaker Atallah <mr.chakerw@gmail.com>
2023-10-14 12:50:12 +0100

    update tab svg

--- src/componants/Tabs.rs
+++ src/componants/Tabs.rs
@@ -1,25 +1,30 @@
-use leptos::{*,  html::Div};
+use leptos::{*,  html::Div, html::Svg};
 use web_sys::Node;
 
 #[component]
 fn LabelsView(cx: Scope, vec: Vec<&'static str>, selected_tab: ReadSignal<usize>, set_selected_tab: WriteSignal<usize>) -> impl IntoView {
     
-    
+    let (_vec, set_vec) = create_signal(cx, vec);
     view! {cx, 
-            <img  on:click=move |_|{
-            if selected_tab() != 0 {
-                set_selected_tab( selected_tab() - 1 )
-            }}
-            src="/images/left.svg"
-            class="tab cursor-pointer" 
-            />
-            <img  on:click=move |_|{
-                if selected_tab() != vec.len() - 1 {
+            <svg width="43" height="43" viewBox="0 0 43 43" fill="none" xmlns="http://www.w3.org/2000/svg" class="tab cursor-pointer" 
+                on:click=move |_|{
+                    if selected_tab() != 0 {
+                        set_selected_tab( selected_tab() - 1 )
+                    }}
+            >
+                <path d="M35.4941 1H6.65545C3.53203 1 1 3.53203 1 6.65545V35.4941C1 38.6175 3.53203 41.1495 6.65545 41.1495H35.4941C38.6175 41.1495 41.1495 38.6175 41.1495 35.4941V6.65545C41.1495 3.53203 38.6175 1 35.4941 1Z" fill=move || format!("{}", if selected_tab() != 0 {"#EEFFAA"} else { "#888888" }) fill-opacity="0.4" stroke="black" stroke-width="2" stroke-miterlimit="2"/>
+                <path d="M8 21L18 26.7735V15.2265L8 21ZM17 22H34V20H17V22Z" fill="black"/>
+            </svg>
+            <svg width="43" height="43" viewBox="0 0 43 43" fill="none" xmlns="http://www.w3.org/2000/svg" class="tab cursor-pointer rotate-180" 
+            on:click=move |_|{
+                if selected_tab() != _vec().len() - 1 {
                     set_selected_tab( selected_tab() + 1 )
                 }}
-                src="/images/left.svg"
-                class="tab cursor-pointer rotate-180" 
-            /> 
+            >
+                <path d="M35.4941 1H6.65545C3.53203 1 1 3.53203 1 6.65545V35.4941C1 38.6175 3.53203 41.1495 6.65545 41.1495H35.4941C38.6175 41.1495 41.1495 38.6175 41.1495 35.4941V6.65545C41.1495 3.53203 38.6175 1 35.4941 1Z" fill=move || format!("{}", if selected_tab() !=  _vec().len() - 1 {"#EEFFAA"} else { "#888888" }) fill-opacity="0.4" stroke="black" stroke-width="2" stroke-miterlimit="2"/>
+                <path d="M8 21L18 26.7735V15.2265L8 21ZM17 22H34V20H17V22Z" fill="black"/>
+            </svg>
+           
         }
 }
 
