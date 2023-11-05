b55bb49062f6e5fd042bef14f0810d58f5780ca5
MrChaker <mr.chakerw@gmail.com>
2023-11-05 19:40:50 +0100

    change mathjax config

--- src/app.rs
+++ src/app.rs
@@ -36,12 +36,75 @@ pub fn App(cx: Scope) -> impl IntoView {
       // sets the document title
       <Title text="Little Bo Peep"/>
       <Link href="/images/book_favicon_sized_v2.png" rel="icon"/>
-      <Script src="/mathjax_setup.js" defer="true"/>
       <Script
-        src="https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.7/MathJax.js?config=TeX-AMS_SVG"
-        type_="text/javascript"
-        defer="true"
+      src="https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.7/MathJax.js?config=TeX-AMS_SVG"
+      type_="text/javascript"
       />
+      <script type_="text/x-mathjax-config">
+        r#"
+        MathJax.Hub.Config({
+          showProcessingMessages: false, // Disables the display of "Processing Math: x%" messages
+          messageStyle: "none", // No message display
+          SVG: {
+            mtextFontInherit: true, // Ensures mtext tags use the surrounding font
+          },
+          jax: ["input/TeX", "output/SVG"],
+          tex2jax: {
+            inlineMath: [
+              ["$", "$"],
+              ["\\(", "\\)"]
+            ],
+            processEscapes: true
+          },
+          TeX: {
+            Macros: {
+              dblcol: "\\!\\rt{0.1}\\mathrel{\\raise.13ex{\\substack{\\small \\circ \\\\ \\small \\circ}}}",
+              hc: "\\!\\rt{0.1}\\mathrel{\\raise.13ex{\\substack{\\small \\circ \\\\ \\small \\circ}}}",
+              rr: "\\mathbb{R}",
+              zz: "\\mathbb{Z}",
+              nn: "\\mathbb{N}",
+              ww: "\\mathbb{W}",
+              qq: "\\mathbb{Q}",
+              te: "\\text",
+              dom: "\\text{dom}\\,",
+              degree: "\\text{deg}\\,",
+              f: "\\Rule{0.12em}{0.8pt}{-0.8pt}f",
+              fsp: "\\hspace{0.06em}\\Rule{0.12em}{0.8pt}{-0.8pt}f",
+              sp: "\\Rule{0.08em}{0.8pt}{-0.8pt}",
+              ra: "\\rightarrow",
+              back: "\\backslash",
+              sqt: "{\\color{white} *\\!\\!\\!}",
+              up: ["\\rule{0pt}{#1em}", 1],
+              dn: ["\\Rule{0pt}{0em}{#1em}", 1],
+              rt: ["\\hspace{#1em}", 1],
+              hlfbk: "\\!\\hspace{0.1em}",
+              fl: ["\\lfloor #1 \\rfloor", 1],
+              cl: ["\\lceil #1 \\rceil", 1],
+              FL: ["\\left\\lfloor #1 \\right\\rfloor", 1],
+              CL: ["\\left\\lceil #1 \\right\\rceil", 1],
+              implies: "\\Longrightarrow",
+              psa: "{}\\!\\hspace{0.0691em}",
+              psb: "{}\\!\\hspace{0.06901311249137em}",
+              ncdot: "\\cdot",
+              re: "\\!",
+              bk: "\\!",
+              gbk: "\\!\\hspace{0.15em}",
+              fw: "\\hspace{0.1em}",
+              hfbk: "\\!\\hspace{0.2em}",
+              deg: "\\circ",
+              km: "[\\text{km}]",
+              ddx: "{d \\over dx}\\hspace{0.1em}",
+              ddt: "{d \\over dt}\\hspace{0.1em}",
+              ddu: "{d \\over du}\\hspace{0.1em}",
+              ddz: "{d \\over dz}\\hspace{0.1em}",
+              ov: ["\\overline{#1}", 1],
+              floor: ["\\lfloor{#1}\\rfloor", 1],
+              faketextelement: "{\\color{white}\\text{*}}\\!\\!\\!\\rt{0.1}",
+            }
+          }
+        });
+        "#
+      </script>
 
       // content for this welcome page
       <Router fallback=|cx| {
