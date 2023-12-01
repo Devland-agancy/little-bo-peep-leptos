6a9afc88c9bea3f95f8a05e2d6d48f63537a768e
MrChaker <mr.chakerw@gmail.com>
2023-12-01 17:00:35 +0100

    Add chapter 2 content

--- src/page/article/ch_2.rs
+++ src/page/article/ch_2.rs
@@ -29,68 +29,80 @@ fn ArticleBody(cx: Scope) -> impl IntoView {
     elm_to_view! {
       cx,
       r#"
-      |> Paragraph 
+        |> Paragraph 
 
-          *Slopes.*
-          the %slope% of a line is a mathematical measure of how
-          “steep” a line is.
-          Here are a few examples (for an explanation of the values,
-          see below):
+            *Slopes.*
+            the %slope% of a line is a mathematical measure of how
+            “steep” a line is.
+            Here are a few examples (for an explanation of the values,
+            see below):
 
-      |> Image
-          src="/images/chapter_2_1.svg"
-          width=450
-          height=890
-          id="test-bg"
+        |> Image
+            src="/images/chapter_2_1.svg"
+            width=450
+            height=890
+            id="test-bg"
 
-      |> Paragraph    
-          
-          To explain now, The slope of a line is...
+        |> Paragraph    
+            
+            To explain now, The slope of a line is...
 
-      |> Paragraph   
-          margin_top = 15
-          align = Align::Center
+        |> Paragraph   
+            margin_top = 15
+            align = Align::Center
 
-          %the number of units the line goes up with each unit to the right%
-          
-      |> Paragraph
-          margin_top = 15
+            %the number of units the line goes up with each unit to the right%
+            
+        |> Paragraph
+            margin_top = 15
 
-          ...assuming that numbers on the $y$-axis increase going up and that 
-          numbers on the $x$-axis increase going right, as is usually the case.
-          One can also describe slope as...
+            ...assuming that numbers on the $y$-axis increase going up and that 
+            numbers on the $x$-axis increase going right, as is usually the case.
+            One can also describe slope as...
 
-      |> Paragraph   
-          margin_top = 15
-          align = Align::Center
-          
-          %the amount of vertical change per unit of horizontal change%
+        |> Paragraph   
+            margin_top = 15
+            align = Align::Center
+            
+            %the amount of vertical change per unit of horizontal change%
 
-      |> Paragraph   
-          margin_top = 15
-          
-          ...more elegantly.
+        |> Paragraph   
+            margin_top = 15
+            
+            ...more elegantly.
 
-      |> Paragraph   
-          indent = Indent::Line
+        |> Paragraph   
+            indent = Indent::Line
 
-          Thus, for example, the line below has slope 1 because it goes up by $1$ unit for each unit to the right:
+            Thus, for example, the line below has slope 1 because it goes up by $1$ unit for each unit to the right:
 
-      |> Image
-          width = 363 
-          height = 330 
-          src="/images/slope_1_example.svg"
+        |> Image
+            width = 363 
+            height = 330 
+            src="/images/slope_1_example.svg"
 
-      |> Paragraph   
+        |> Paragraph   
 
-          On the other hand, the line below has slope $-0.5$, because it goes up 
-          by %minus% $0.5$ units with each unit to the right:
-  
-      |> Image
-          width = 363 
-          height = 330 
-          src="/images/slope_0.5_example.svg"
+            On the other hand, the line below has slope $-0.5$, because it goes up 
+            by %minus% $0.5$ units with each unit to the right:
+    
+        |> Image
+            width = 363 
+            height = 330 
+            src="/images/slope_0.5_example.svg"
 
+        |> Paragraph   
+
+            (Etc.)
+
+        |> Paragraph   
+            margin_top = 15
+
+            *Measuring Slope.*
+            The slope of a line is also the ratio of vertical change
+            to horizontal change between any two distinct points $A$, $B$
+            on the line:
+            
       "#
     }
 }
