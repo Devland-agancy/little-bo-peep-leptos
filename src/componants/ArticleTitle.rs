16a4101228fb49d59dd7643ef9aa252cc44995df
MrChaker <mr.chakerw@gmail.com>
2023-12-10 20:53:48 +0100

    #LBP-162 add margin to title

--- src/componants/ArticleTitle.rs
+++ src/componants/ArticleTitle.rs
@@ -8,7 +8,7 @@ pub fn ArticleTitle(
 ) -> impl IntoView {
     view! { cx,
       <div class="sm:grid gridColsWidth">
-        <h1 class="sm:col-start-2 text-3xl sm:text-4xl p-4">
+        <h1 class="sm:col-start-2 text-3xl sm:text-4xl p-4 mt-2 sm:mt-0">
           <span class="sm:hidden">{if on_mobile == "" {label} else {on_mobile}}</span>
           <span class="hidden sm:block">{label}</span>
         </h1>
