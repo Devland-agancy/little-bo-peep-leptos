0eb6d1673cf78a32973133b807bec90676256ff4
MrChaker <mr.chakerw@gmail.com>
2023-12-11 21:42:54 +0100

    #LBP-170 change cream svg positions

--- src/page/article/ch_1.rs
+++ src/page/article/ch_1.rs
@@ -337,7 +337,7 @@ fn ArticleBody(cx: Scope) -> impl IntoView {
           <Span bold=true>"Example 1. "</Span>
           "One has"
         </Paragraph>
-        <MathBlock height=Height::Fit arrow_position="4.7rem 0rem auto auto">
+        <MathBlock height=Height::Fit arrow_position="3.8rem -0.5rem auto auto">
           r#"$$
               \begin{align}
               (10 + 2)(10 + 4) \,&=\, 10\!\cdot\!10 \,+\, 10\!\cdot\!4 \,+\, 2\!\cdot\!10 \,+\, 2\!\cdot\!4\\
@@ -351,7 +351,7 @@ fn ArticleBody(cx: Scope) -> impl IntoView {
           <Span bold=true>"Example 2. "</Span>
           "One has"
         </Paragraph>
-        <MathBlock height=Height::Fit arrow_position="4.7rem 0rem auto auto">
+        <MathBlock height=Height::Fit arrow_position="3.8rem -0.5rem auto auto">
           r#"$$
               \begin{align}
               (10 + 3)(10 + 3) \,&=\, 10\!\cdot\!10 \,+\, 10\!\cdot\!3 \,+\, 3\!\cdot\!10 \,+\, 3\!\cdot\!3\\
@@ -652,7 +652,7 @@ fn ArticleBody(cx: Scope) -> impl IntoView {
                 <Math>r#"$ \sqrt[3]{\up{0.75}\sqrt[2]{2}} $"#</Math> " are both equal to "
                 <Math>r#"$ \sqrt[6]{\up{0.6}2} $"#</Math> ". To convince yourself, note that"
               </Paragraph>
-              <MathBlock id="sqrt-1" height=Height::Fit arrow_position="9.8rem 0rem auto auto">
+              <MathBlock id="sqrt-1" height=Height::Fit arrow_position="8.8rem 0rem auto auto">
                 r#"$$
                     \begin{align}
                     &\,\, (\sqrt[2]{\up{0.75}\sqrt[3]{2}}\rt{0.1})^6 \\
@@ -687,7 +687,7 @@ fn ArticleBody(cx: Scope) -> impl IntoView {
                 </ImageLeft>
               </MathBlock>
               <Paragraph>"and"</Paragraph>
-              <MathBlock id="sqrt-2" height=Height::Fit arrow_position="9.8rem 0rem auto auto">
+              <MathBlock id="sqrt-2" height=Height::Fit arrow_position="8.8rem 0rem auto auto">
                 r#"$$
                     \begin{align}
                     &\,\, (\sqrt[3]{\up{0.75}\sqrt[2]{2}}\rt{0.1})^6 \\
@@ -988,7 +988,7 @@ fn ArticleBody(cx: Scope) -> impl IntoView {
               <Paragraph margin_top=15>
                 "h. (True) Here are the first few powers of $-1$ (note how each additional multiplication by $-1$ simply changes the sign of the previous result):"
               </Paragraph>
-              <MathBlock height=Height::Fit child_tag="span" arrow_position="14.3rem 0rem 0rem 90%">
+              <MathBlock height=Height::Fit child_tag="span" arrow_position="13.3rem 0rem 0rem 90%">
                 <div class="w-full">
                   <Grid margin_top=15 margin_bottom=15 cols=3 place_items="end" classes="w-fit">
 
