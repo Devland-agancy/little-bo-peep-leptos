c13aa0e32ffddc2570ee34b8d8ebf0a45fd04667
MrChaker <mr.chakerw@gmail.com>
2023-12-16 23:07:58 +0100

    #LBP-188

--- src/constants/mod.rs
+++ src/constants/mod.rs
@@ -2,3 +2,6 @@ pub const MENU_ITEMS: &'static [(&'static str, &'static str, &'static str)] = &[
     ("Chapter 1: A Few Refreshers", "", "ch_1"),
     ("Chapter 2: The Slope Formula", "Chapter 2: Slopes", "ch_2"),
 ];
+
+//first value is for top second for bottom
+pub const TITLE_MARGIN: &'static (&'static str, &'static str) = &("15px", "5px");
