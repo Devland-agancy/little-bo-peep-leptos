56a99845cd5c168a9185a9f89a5e271f6854e844
MrChaker <mr.chakerw@gmail.com>
2023-12-22 23:36:22 +0100

    Fix scroll listener not activated of fresh start

--- public/mathjax.js
+++ public/mathjax.js
@@ -1,4 +1,5 @@
 localStorage.setItem("activate_scroll", "false");
+
 if (localStorage.getItem(`${location.pathname.split("/")[2]}_scroll`)) {
   setTimeout((e) => {
     window.scroll({
@@ -8,33 +9,30 @@ if (localStorage.getItem(`${location.pathname.split("/")[2]}_scroll`)) {
 }
 
 MathJax.Hub.Queue(["Typeset", MathJax.Hub]);
-
 MathJax.Hub.Register.StartupHook("End", function () {
   document.querySelectorAll(".hidden-on-startup").forEach((elem) => {
     elem.classList.remove("hidden-on-startup");
     elem.classList.add("animate-appear");
-
-    setTimeout((e) => {
-      if (localStorage.getItem(`${location.pathname.split("/")[2]}_scroll`)) {
-        window.scroll({
-          top: localStorage.getItem(
-            `${location.pathname.split("/")[2]}_scroll`
-          ),
-        });
-      }
-      setTimeout((e) => {
-        localStorage.setItem("activate_scroll", "true");
-        window.addEventListener("scroll", () => {
-          if (
-            localStorage.getItem("activate_scroll") == "true" &&
-            window.scrollY > 0
-          )
-            localStorage.setItem(
-              `${location.pathname.split("/")[2]}_scroll`,
-              window.scrollY
-            );
-        });
-      }, 100);
-    }, 700);
   });
+
+  setTimeout((e) => {
+    if (localStorage.getItem(`${location.pathname.split("/")[2]}_scroll`)) {
+      window.scroll({
+        top: localStorage.getItem(`${location.pathname.split("/")[2]}_scroll`),
+      });
+    }
+    setTimeout((e) => {
+      localStorage.setItem("activate_scroll", "true");
+      window.addEventListener("scroll", () => {
+        if (
+          localStorage.getItem("activate_scroll") == "true" &&
+          window.scrollY > 0
+        )
+          localStorage.setItem(
+            `${location.pathname.split("/")[2]}_scroll`,
+            window.scrollY
+          );
+      });
+    }, 100);
+  }, 700);
 });
--- public/mathjax_setup.js
+++ public/mathjax_setup.js
@@ -74,30 +74,32 @@ MathJax.Hub.Register.StartupHook("End", function () {
   document.querySelectorAll(".hidden-on-startup").forEach((elem) => {
     elem.classList.remove("hidden-on-startup");
     elem.classList.add("animate-appear");
-
-    setTimeout((e) => {
-      if (localStorage.getItem(`${location.pathname.split("/")[2]}_scroll`)) {
-        window.scroll({
-          top: localStorage.getItem(
-            `${location.pathname.split("/")[2]}_scroll`
-          ),
-        });
-      }
-      setTimeout((e) => {
-        window.addEventListener("scroll", () => {
-          if (
-            window.scrollY > 0 &&
-            localStorage.getItem("activate_scroll") == "true"
-          )
-            localStorage.setItem(
-              `${location.pathname.split("/")[2]}_scroll`,
-              window.scrollY
-            );
-        });
-      }, 100);
-    }, 400);
   });
+
   setTimeout((e) => {
     document.getElementsByTagName("body").item(0).style.opacity = 1;
+
+    if (localStorage.getItem(`${location.pathname.split("/")[2]}_scroll`)) {
+      window.scroll({
+        top: localStorage.getItem(`${location.pathname.split("/")[2]}_scroll`),
+      });
+    }
+    setTimeout((e) => {
+      window.addEventListener("scroll", () => {
+        if (
+          !localStorage.getItem(`${location.pathname.split("/")[2]}_scroll`)
+        ) {
+          localStorage.setItem("activate_scroll", "true");
+        }
+        if (
+          window.scrollY > 0 &&
+          localStorage.getItem("activate_scroll") == "true"
+        )
+          localStorage.setItem(
+            `${location.pathname.split("/")[2]}_scroll`,
+            window.scrollY
+          );
+      });
+    }, 100);
   }, 400);
 });
