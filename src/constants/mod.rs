67adb62e1f76552c037d3220cfbd71ea26e35424
MrChaker <mr.chakerw@gmail.com>
2023-12-22 11:08:02 +0100

    Update constatns file

--- src/constants/mod.rs
+++ src/constants/mod.rs
@@ -9,3 +9,7 @@ pub const TITLE_MARGIN: &'static (&'static str, &'static str) = &("15px", "5px")
 pub const SHOW_CLICKABLE_ITEMS_BORDERS: bool = true;
 
 pub const GREEN_DIV_HEIGHT: u8 = 150;
+
+// the max width of the main column of text when trying to fit a small screen, in other words the breakpoint of screen width that activates small screen mode , this can be edited in tailwind.config.js in here screens: { sm: "520px" }, after than run npx tailwindcss -i input.css -o style/output.css
+
+// DESKTOP_TEXT_WIDTH found in input.css file in .gridColWidth class , 1fr {width} 1fr
