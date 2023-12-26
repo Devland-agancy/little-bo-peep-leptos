dc13ba301efc2a8ebecb0b87727f3fe807b0ed16
MrChaker <mr.chakerw@gmail.com>
2023-12-26 22:26:03 +0100

    #LBP-199 increase squiggle clickable area

--- src/componants/ImageLeft.rs
+++ src/componants/ImageLeft.rs
@@ -73,10 +73,10 @@ pub fn ImageLeft(
 
         <Show fallback=|_| () when=move || hidden_in_mobile>
           <div
-            class="block sm:hidden absolute p-8"
+            class="block sm:hidden absolute"
             class=("outline", move || SHOW_CLICKABLE_ITEMS_BORDERS)
             class=("outline-blue-300", move || SHOW_CLICKABLE_ITEMS_BORDERS)
-            style=move || format!("right: calc({} - 30px); top: calc({} - 30px)", squiggle_right, squiggle_top)
+            style=move || format!("right: calc({} - 40px); top: calc({} - 40px); padding: {}", squiggle_right, squiggle_top, "2.6rem")
           >
             <img src="/images/squiggle.png" class="h-11"/>
           </div>
--- src/componants/ImageRight.rs
+++ src/componants/ImageRight.rs
@@ -84,10 +84,10 @@ pub fn ImageRight(
 
         <Show fallback=|_| () when=move || hidden_in_mobile>
           <div
-            class="block sm:hidden absolute p-8"
+            class="block sm:hidden absolute"
             class=("outline", move || SHOW_CLICKABLE_ITEMS_BORDERS)
             class=("outline-blue-300", move || SHOW_CLICKABLE_ITEMS_BORDERS)
-            style=move || format!("left: calc({} - 30px); top: calc({} - 30px)", squiggle_left, squiggle_top)
+            style=move || format!("left: calc({} - 40px); top: calc({} - 40px); padding: {}", squiggle_left, squiggle_top, "2.6rem")
           >
             <img src="/images/squiggle.png" class="h-11"/>
           </div>
