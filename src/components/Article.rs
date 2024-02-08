c4516183eb07bc0d696a66120528f531f09212e6
MrChaker <mr.chakerw@gmail.com>
2024-02-08 17:23:16 +0100

    New margin mode mechanism

--- src/components/Article.rs
+++ src/components/Article.rs
@@ -1,13 +1,12 @@
 use crate::{global_state::GlobalState, page::state::PageState};
 use leptos::{
-    ev::{click, mousedown, touchstart, EventDescriptor},
+    ev::{click, scrollend, touchend},
     html::Div,
     *,
 };
 use leptos_use::use_event_listener;
-use std::cmp;
 use std::time::Duration;
-use web_sys::{Event, ScrollBehavior, ScrollToOptions};
+use web_sys::{ScrollBehavior, ScrollToOptions};
 
 #[component]
 pub fn Article(cx: Scope, children: Children) -> impl IntoView {
@@ -23,19 +22,35 @@ pub fn Article(cx: Scope, children: Children) -> impl IntoView {
     } = use_context::<GlobalState>(cx).unwrap();
     // can_click is for disabling click on page transition
     let (can_click, set_can_click) = create_signal(cx, true);
+    let (state_changed_by_scroll, set_state_changed_by_scroll) = create_signal(cx, false);
 
     create_effect(cx, move |_| {
-        let mut options = ScrollToOptions::new();
-
-        if show_right() || show_left() {
-            options.behavior(ScrollBehavior::Smooth);
-            if article_node().is_some() {
-                let _ = article_node().unwrap().style("left", "1500px");
-            }
+        if show_article() {
+            let mut options = ScrollToOptions::new();
             options.left(1500.0);
             options.behavior(ScrollBehavior::Instant);
             window().scroll_with_scroll_to_options(&options);
+
+            let scroll_back = move || {
+                let mut options = ScrollToOptions::new();
+                set_can_click(true);
+
+                if window().scroll_x().unwrap() > 1000.0 && window().scroll_x().unwrap() < 2000.0 {
+                    options.left(1500.0);
+                    options.behavior(ScrollBehavior::Smooth);
+                    window().scroll_with_scroll_to_options(&options);
+                    return ();
+                } else {
+                    set_state_changed_by_scroll(true);
+                }
+            };
+
+            let _ = use_event_listener(cx, document(), scrollend, move |_| scroll_back());
+            let _ = use_event_listener(cx, document(), touchend, move |_| scroll_back());
         }
+    });
+    create_effect(cx, move |_| {
+        let mut options = ScrollToOptions::new();
 
         if show_right() {
             set_can_click(false);
@@ -48,7 +63,9 @@ pub fn Article(cx: Scope, children: Children) -> impl IntoView {
                 },
                 Duration::from_millis(100),
             );
-        } else if show_left() {
+            return;
+        }
+        if show_left() {
             set_timeout(
                 move || {
                     options.left(if margin_scroll_value.get() == 0.0 {
@@ -74,13 +91,9 @@ pub fn Article(cx: Scope, children: Children) -> impl IntoView {
                 window().scroll_with_scroll_to_options(&options);
                 set_timeout(
                     move || {
-                        let _ = article_node().unwrap().style("left", "0");
-                        options.behavior(ScrollBehavior::Instant);
-                        options.left(0.0);
-                        window().scroll_with_scroll_to_options(&options);
                         set_timeout(
                             move || {
-                                set_page_state.update(|value| *value = PageState::ShowArticle);
+                                set_page_state(PageState::ShowArticle);
                                 set_can_click(true);
                             },
                             Duration::from_millis(700),
@@ -89,15 +102,22 @@ pub fn Article(cx: Scope, children: Children) -> impl IntoView {
                     Duration::from_millis(800),
                 );
             }
+            if state_changed_by_scroll() {
+                let mut options = ScrollToOptions::new();
+                options.behavior(ScrollBehavior::Smooth);
+                options.left(1500.0);
+                window().scroll_with_scroll_to_options(&options);
+                set_state_changed_by_scroll(false)
+            }
         });
     });
     // for right_images we autoscroll to their position
     view! { cx,
-      <div class="overscroll-none">
+      <div class="">
         <div
           node_ref=article_node
-          class="relative flex justify-center align-center w-full pb-14 min-h-screen"
-          class=("overflow-hidden", show_article)
+          class="relative flex justify-center align-center w-full pb-14 min-h-screen left-[1500px]"
+          class=("", show_article)
           id="Article"
         >
           <div class="w-full transition duration-300 sm:overflow-visible sm:translate-x-0">
@@ -143,9 +163,9 @@ pub fn ColumnButtonRight(cx: Scope) -> impl IntoView {
         class=("opacity-0", show_article)
         class=("pointer-events-none", show_article)
         class=("opacity-100", show_left)
-        class=("-translate-x-3/4", show_left)
-        class=("lg:-translate-x-[85%]", show_left)
-        class=("opacity-100", show_right)
+        class=("-translate-x-3/4", || true)
+        class=("lg:-translate-x-[85%]", || true)
+        class=("opacity-100", || true)
 
       ></div>
     }
@@ -166,11 +186,11 @@ pub fn ColumnButtonLeft(cx: Scope) -> impl IntoView {
         class=("opacity-0", show_article)
         class=("pointer-events-none", show_article)
         class=("opacity-100", show_right)
-        class=("-translate-x-3/4", show_right)
+        class=("-translate-x-3/4", || true)
         class=("lg:-translate-x-[85%]", show_right)
-        class=("opacity-100", show_left)
+        class=("opacity-100", || true)
         class=("translate-x-3/4", show_left)
-        class=("lg:translate-x-[85%]", show_left)
+        class=("lg:translate-x-[85%]", || true)
       ></div>
     }
 }
