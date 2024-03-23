e4ffcb3cc6ca1259ed7ee221d6897625196c10af
MrChaker <mr.chakerw@gmail.com>
2024-03-23 12:23:17 +0100

    Fix indents #LBP-296

--- src/components/Indent.rs
+++ src/components/Indent.rs
@@ -1,10 +1,10 @@
-use leptos::*;
+use leptos::{html::Span, *};
 
 #[component]
 pub fn Indent(cx: Scope, children: Children) -> impl IntoView {
     view! { cx,
       <span
-        class="indent-10 inline-table -mt-4"
+        class="indent indent-10 block"
       >
         {children(cx)}
       </span>
