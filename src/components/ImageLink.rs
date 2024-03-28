7f6df663d9682892460a363f6ef6344c96261d55
MrChaker <mr.chakerw@gmail.com>
2024-03-28 11:58:33 +0100

    #LBP-301 handle prop quotes in parser

--- src/components/ImageLink.rs
+++ src/components/ImageLink.rs
@@ -1,15 +1,10 @@
 use crate::{global_state::GlobalState, page::state::PageState};
 use leptos::{html::Img, *};
 
-pub enum Direction {
-    Left,
-    Right,
-}
-
 #[component]
 pub fn ImageLink(
     cx: Scope,
-    direction: Direction,
+    direction: &'static str,
     scroll_by: f64,
     children: Children,
 ) -> impl IntoView {
@@ -28,16 +23,17 @@ pub fn ImageLink(
           return;
           if page_state() == PageState::ShowArticle {
             match direction {
-              Direction::Left => {
+              "left" => {
                 set_page_state(PageState::ShowLeft);
                 margin_scroll_value
                     .set(scroll_by);
               },
-              Direction::Right => {
+              "right" => {
                 set_page_state(PageState::ShowRight);
                 margin_scroll_value
                     .set(200.0);
-              }
+              },
+              _ => {}
             }
           }
         }
