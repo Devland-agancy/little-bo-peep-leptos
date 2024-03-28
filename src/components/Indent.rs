99edf641bc6916078ba296f5e2b4a4149e7e8628
MrChaker <mr.chakerw@gmail.com>
2024-03-28 12:08:29 +0100

    Fix indent margin

--- src/components/Indent.rs
+++ src/components/Indent.rs
@@ -7,15 +7,17 @@ pub fn Indent(cx: Scope, children: Children) -> impl IntoView {
 
     create_effect(cx, move |_| {
         if let Some(node) = node_ref() {
-            let class = Array::of1(&"-mt-4".into());
-            let _ = node.parent_element().unwrap().class_list().add(&class);
+            let class = Array::of1(&"!-mt-4".into());
+            if let Some(parent) = node.parent_element() {
+                let _ = parent.class_list().add(&class);
+            }
         }
     });
 
     view! { cx,
       <span
         node_ref=node_ref
-        class="indent indent-10 inline-table"
+        class="indent-10 block"
       >
         {children(cx)}
       </span>
