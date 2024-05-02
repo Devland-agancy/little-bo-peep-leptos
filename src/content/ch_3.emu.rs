b5107b715af22c9c7a77ac98bafdc255dfab014e
JOHN STEINBERGER <jpsteinb@gmail.com>
2024-05-02 16:15:57 +0200

    first bit of chapter 3

--- src/content/ch_3.emu.rs
+++ src/content/ch_3.emu.rs
@@ -11,7 +11,7 @@
     “input tube” and an “output tube”:
 
     |> Image
-        src ch3figs/66.svg
+        src images/66.svg
 
     An input goes in via the input tube, is
     processed according to the function's rule,
@@ -19,7 +19,7 @@
     (Metaphorically speaking.)
 
     In the above picture, the name of the function
-    is “ $f$.
+    is “$f$.
     
     Notation-wise, one writes
 
@@ -63,3 +63,76 @@
     __definition__
     
     of $f$!! (Stated algebraically.)
+
+|> Section
+
+    *Lambda functions.*
+    A
+
+    __lambda function__
+
+    is not a type of function, but a type of
+    notation  that enables one to 
+    define a function without giving it a name, 
+    such as “$f$”. In fact there are two different 
+    mainstream notations, in this instance.
+    One notation writes
+    $$
+    \lambda{x}.x^2
+    $$
+    to mean 
+    “the function that maps $x$ to $x^2$” 
+    (and by the way,
+    $$
+    \lambda{z}.z^2
+    $$
+    is <i>the same</i> function, because 
+    it specifies the same in-out mapping—a thing 
+    goes to its square—also by the way, the symbol
+    $$
+    {\Huge \lambda}
+    $$
+    is the Greek letter “lambda”, giving its name to 
+    the topic) while the other notation writes 
+    $$
+    x \rightarrow x^2
+    $$
+    to mean the same thing.
+
+    Note that
+
+    $$
+    (x \ra x^2)(0.1)
+    $$
+
+    means “the function that maps each number to its 
+    square, of $0.1$”. So...
+
+    $$
+    (x \ra x^2)(0.1) = 0.01
+    $$
+
+    ...the same as an equation of the form 
+    “$f(\dots) = \dots$
+
+    For more practice:
+
+    $$
+    (\lambda x.x^3)(10) = 1000
+    $$
+    $$
+    (\lambda u.u^5)(10) = 100000
+    $$
+    $$
+    (v \ra v^2)(10) = 100
+    $$
+    $$
+    (z \ra z^3)(10) + (t \ra t^2)(5) = 1025.
+    $$
+
+    (Etc.) (Indeed, to emphasize again, the variable 
+    denoting the input does not matter: it is just a 
+    placeholder, and you obtain the same output, and 
+    the same <i>function</i>, no matter what symbol 
+    you choose.*) (*As long as you don't collide with 
+    other existing variable names.)
