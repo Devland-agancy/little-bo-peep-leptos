302819884297a29e54a3bc4311f4758dfc1f6280
JOHN STEINBERGER <jpsteinb@gmail.com>
2024-05-02 15:52:44 +0200

    first

--- src/content/ch_3.emu.rs
+++ src/content/ch_3.emu.rs
@@ -1,55 +1,65 @@
 |> Section
-
-    *Measuring Slope.*
-    The slope of a line is also the ratio of vertical change
-    to horizontal change between any two distinct points
-    $A$, $B$ on the line:
-
-    *Measuring Slope.*
-    The slope of a line is also the ratio of vertical change
-    to horizontal change between any two distinct points
-    $A$, $B$ on the line:
-
-    *Measuring Slope.*
-    The slope of a line is also the ratio of vertical change
-    to horizontal change between any two distinct points
-    $A$, $B$ on the line:
-
-    *Measuring Slope.*
-    The slope of a line is also the ratio of vertical change
-    to horizontal change between any two distinct points
-    $A$, $B$ on the line:
     
-    |> ImageRight
-        src images/32.svg
-        line 0.5
-        offset_y -1rem
-        use_squiggle_on_mobile false
-        squiggle_y 47%
+    *Syntax.*
+    A
 
-    The slope of a line is also the ratio of vertical change
-    to horizontal change between any two distinct points
-    $A$, $B$ on the line:
-    The slope of a line is also the ratio of vertical change
-    to horizontal change between any two distinct points
-    $A$, $B$ on the line:
-    The slope of a line is also the ratio of vertical change
-    to horizontal change between any two distinct points
-    $A$, $B$ on the line:
-    The slope of a line is also the ratio of vertical change
-    to horizontal change between any two distinct points
-    $A$, $B$ on the line:
-    The slope of a line is also the ratio of vertical change
-    to horizontal change between any two distinct points
-    $A$, $B$ on the line:
+    __function__
 
-    *Measuring Slope.*
-    The slope of a line is also the ratio of vertical change
-    to horizontal change between any two distinct points
-    $A$, $B$ on the line:
+    is a “rule” for transforming inputs (usually
+    numbers) into outputs (usually numbers as well). 
+    One can think of a function as a box with an 
+    “input tube” and an “output tube”:
+
+    |> Image
+        src ch3figs/66.svg
+
+    An input goes in via the input tube, is
+    processed according to the function's rule,
+    and the result comes out the other side.
+    (Metaphorically speaking.)
+
+    In the above picture, the name of the function
+    is “ $f$.
     
-    |> ImageLeft
-        src images/35.svg
-        y top
-        offset_x -4rem
-        use_squiggle_on_mobile false
+    Notation-wise, one writes
+
+    $$
+    {f(x)}
+    $$
+
+    (which is read “$f$ of $x$”, and that's 
+    important) for the result of passing an input $x$ 
+    to a function $f$. For example, if the rule 
+    according to which $f$ processes inputs is
+
+    __the output is the square of the input__
+
+    then
+
+    $$
+    {f(2) = 4}
+    $$
+
+    [“$f$ of $2$ equals $4$”] because $2^2 = 4$, and
+
+    $$
+    {f(3) = 9}
+    $$
+
+    [“$f$ of $3$ equals $9$”] because $3^2 = 9$, and
+    $$
+    { f(0.1) = 0.01}
+    $$
+
+    [...] because $0.1^2 = 0.01$, and so on. Also,
+
+    $$
+    {f(x) = x^2}
+    $$
+
+    [“$f$ of $x$ equals $x^2$”] more generally, which 
+    is actually the
+
+    __definition__
+    
+    of $f$!! (Stated algebraically.)
