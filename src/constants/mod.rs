129c601662f69cd7ff04c3e5aa01f1a2d43f3e33
MrChaker <mr.chakerw@gmail.com>
2024-05-07 10:24:11 +0100

    #LBP-318

--- src/constants/mod.rs
+++ src/constants/mod.rs
@@ -1,6 +1,6 @@
 pub const MENU_ITEMS: &'static [(&'static str, &'static str, &'static str)] = &[
     ("Chapter 1: A Few Refreshers", "", "ch_1"),
-    ("Chapter 2: The Slope Formula", "Chapter 2: Slopes", "ch_2"),
+    ("Chapter 2: Slopes", "", "ch_2"),
     ("Chapter 3: Functions", "", "ch_3"),
 ];
 
