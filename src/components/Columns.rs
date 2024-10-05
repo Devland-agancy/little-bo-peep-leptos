9b8ea2891ebf1c55d14a94c9ee9acbd6fb0fd2b6
MrChaker <mr.chakerw@gmail.com>
2024-10-05 23:48:06 +0100

    #LBP-422

--- src/components/Columns.rs
+++ src/components/Columns.rs
@@ -3,7 +3,7 @@ use leptos::*;
 #[component]
 pub fn Columns(cx: Scope, children: Children) -> impl IntoView {
     view! { cx,
-      <div class="leading-[28px] sm:leading-[32.5px]">
+      <div class="leading-[28px] sm:leading-[32.5px] -mt-4">
         {children(cx)}
       </div>
     }
