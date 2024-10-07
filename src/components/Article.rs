916c29ca6a7966898fd46ce683cd58bceea72695
MrChaker <mr.chakerw@gmail.com>
2024-10-07 14:48:48 +0100

    #LBP-421 Adjust tap to recenter style

--- src/components/Article.rs
+++ src/components/Article.rs
@@ -88,13 +88,13 @@ pub fn Article(cx: Scope, children: Children) -> impl IntoView {
           <ColumnButtonLeft/>
           <ColumnButtonRight/>
           <div
-            class="fixed left-0 bg-[#b5815e] p-2 w-full transition-all duration-500"
+            class="fixed left-0 bg-[#ffb380ff] p-[.2rem] w-full transition-all duration-500"
             class=("bottom-0", move || margin_mode())
-            class=("bottom-[-6%]", move || !margin_mode())
+            class=("bottom-[-2em]", move || !margin_mode())
             >
             <p
-              class="w-fit m-auto font-BowlbyOne"
-            >"tap to recenter"</p>
+              class="w-fit m-auto font-BowlbyOne text-[15px]"
+            >"TAP TO RECENTER"</p>
           </div>
         </div>
       </div>
